49a4701b4e71e2c851d3b609babf9113aa63e6f9
Adam Niederer <adam.niederer@gmail.com>
2017-04-09 00:21:26 -0400

    Add login page

---
+++ src/components/login.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <section>
+    <input class="input" name="user" placeholder="Username"/>
+    <input class="input" name="password" type="password" placeholder ="Password"/>
+    <router-link to="/">
+      <button class="input button flex-right submit">Log In</button>
+    </router-link>
+  </section>
+</template>
+
+<script lang="coffee">
+</script>
+
+<style lang="sass" scoped>
+.input
+  margin-bottom: 1em
+
+section
+  padding: 5em 20%
+</style>
